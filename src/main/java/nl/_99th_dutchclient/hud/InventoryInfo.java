2c641cbf0121eb1efd93a01ecf4ee3f1102514cb
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-05-07 17:22:58 +0200

    Add color to inventory HUD

--- src/main/java/nl/_99th_dutchclient/hud/InventoryInfo.java
+++ src/main/java/nl/_99th_dutchclient/hud/InventoryInfo.java
@@ -7,6 +7,9 @@ import net.minecraft.item.AirItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
+import nl._99th_dutchclient.util.ColorGradient;
+import java.awt.Color;
+import java.lang.Math;
 
 public class InventoryInfo
 {
@@ -26,23 +29,27 @@ public class InventoryInfo
 
     private static void renderMainhand(Minecraft mc, MatrixStack ms, int wh) {
         String itemString = ItemToString(mc.player.getHeldItemMainhand());
-        mc.fontRenderer.drawString(ms, itemString, 1, wh - 51, -1);
+        int itemColor = ItemToColor(mc.player.getHeldItemMainhand());
+        mc.fontRenderer.drawStringWithShadow(ms, itemString, 1, wh - 51, itemColor);
     }
 
     private static void renderOffhand(Minecraft mc, MatrixStack ms, int wh) {
         String itemString = ItemToString(mc.player.getHeldItemOffhand());
-        mc.fontRenderer.drawString(ms, itemString, 1, wh - 41, -1);
+        int itemColor = ItemToColor(mc.player.getHeldItemOffhand());
+        mc.fontRenderer.drawStringWithShadow(ms, itemString, 1, wh - 41, itemColor);
     }
 
     private static void renderArmor(Minecraft mc, MatrixStack ms, int wh) {
         String itemString;
+        int itemColor;
         int j = 0;
 
         Iterable<ItemStack> armor = mc.player.getArmorInventoryList();
         for(ItemStack i : armor) {
             if(!i.isEmpty()){
                 itemString = ItemToString(i);
-                mc.fontRenderer.drawString(ms, itemString, 1, wh - (61 + (j * 10)), -1);
+                itemColor = ItemToColor(i);
+                mc.fontRenderer.drawStringWithShadow(ms, itemString, 1, wh - (61 + (j * 10)), itemColor);
             }
             j++;
         }
@@ -65,5 +72,31 @@ public class InventoryInfo
         }
 
         return s;
+    }
+
+    private static int ItemToColor(ItemStack itemStack) {
+        if(!itemStack.isDamageable()) return -1;
+
+        float dur = itemStack.getMaxDamage() - itemStack.getDamage();
+        float per = (dur / itemStack.getMaxDamage()) * 100;
+
+        if(per < 0) return -1;
+        if(per > 100) return -1;
+        if(per == 100) return new Color(64, 192, 64).getRGB();
+
+        ColorGradient[] ranges = new ColorGradient[] {
+            new ColorGradient(0, 10, new Color(170, 0, 0), new Color(213, 43, 43)),
+            new ColorGradient(10, 20, new Color(213, 43, 43), new Color(255, 85, 85)),
+            new ColorGradient(20, 30, new Color(255, 85, 85), new Color(255, 128, 43)),
+            new ColorGradient(30, 40, new Color(255, 128, 43), new Color(255, 170, 0)),
+            new ColorGradient(40, 50, new Color(255, 170, 0), new Color(255, 213, 43)),
+            new ColorGradient(50, 60, new Color(255, 213, 43), new Color(255, 255, 85)),
+            new ColorGradient(60, 70, new Color(255, 255, 85), new Color(198, 255, 85)),
+            new ColorGradient(70, 80, new Color(198, 255, 85), new Color(142, 255, 85)),
+            new ColorGradient(80, 90, new Color(142, 255, 85), new Color(85, 255, 85)),
+            new ColorGradient(90, 100, new Color(85, 255, 85), new Color(64, 192, 64))
+        };
+
+        return ranges[(int) Math.floor(per / 10)].getColor(per).getRGB();
     }
 }
---
+++ src/main/java/nl/_99th_dutchclient/util/ColorGradient.java
@@ -0,0 +1,37 @@
+package nl._99th_dutchclient.util;
+
+import java.awt.Color;
+
+public class ColorGradient
+{
+    public int lowerBound;
+    public Color lowerColor;
+
+    public int upperBound;
+    public Color upperColor;
+
+    public ColorGradient(int lowerBound, int upperBound, Color lowerColor, Color upperColor) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+
+        this.lowerColor = lowerColor;
+        this.upperColor = upperColor;
+    }
+
+    public Color getColor(int pointer) {
+        return this.getColor((float) pointer);
+    }
+
+    public Color getColor(float pointer) {
+        if(pointer <= this.lowerBound) return this.lowerColor;
+        if(pointer >= this.upperBound) return this.upperColor;
+
+        float f = (pointer - lowerBound) / (upperBound - lowerBound);
+
+        float r = lowerColor.getRed() + f * (upperColor.getRed() - lowerColor.getRed());
+        float g = lowerColor.getGreen() + f * (upperColor.getGreen() - lowerColor.getGreen());
+        float b = lowerColor.getBlue() + f * (upperColor.getBlue() - lowerColor.getBlue());
+
+        return new Color((int) r, (int) g, (int) b);
+    }
+}
