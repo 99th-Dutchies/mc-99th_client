7a27a3329ebfe4179e64193bcd1ffec9d0ce6c27
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-12-03 22:00:09 +0100

    Fix SERVER_JOIN event triggers not firing (fixes #146)

--- src/main/java/net/minecraft/client/network/play/ClientPlayNetHandler.java
+++ src/main/java/net/minecraft/client/network/play/ClientPlayNetHandler.java
@@ -449,8 +449,8 @@ public class ClientPlayNetHandler implements IClientPlayNetHandler
                     eventTrigger.trigger == EventTrigger.Event.WORLD_JOIN) {
                 eventTrigger.handle();
             }
-            this.client.serverJoinEventFlag = false;
         }
+        this.client.serverJoinEventFlag = false;
     }
 
     /**
