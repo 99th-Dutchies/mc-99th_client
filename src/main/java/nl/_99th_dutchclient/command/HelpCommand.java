b1584e8865bd5f6c787a09d7783130e4d0b282d5
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-12-15 20:01:16 +0100

    Add page index to help-command (implements #163)

--- src/main/java/nl/_99th_dutchclient/command/HelpCommand.java
+++ src/main/java/nl/_99th_dutchclient/command/HelpCommand.java
@@ -1,10 +1,8 @@
 package nl._99th_dutchclient.command;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.text.Color;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.*;
+import nl._99th_dutchclient.util.MCStringUtils;
 
 public class HelpCommand extends Command {
     public HelpCommand() {
@@ -14,8 +12,30 @@ public class HelpCommand extends Command {
     @Override
     public void execute(String string) {
         Minecraft mc = Minecraft.getInstance();
+        String[] arguments = string.split(" ");
+        int maxPages = (int) Math.ceil(mc.commandManager.commands.size() / 10.0);
+        int pageIndex;
 
-        for(Command command : mc.commandManager.commands) {
+        if(arguments.length != 2) {
+            pageIndex = 0;
+        } else {
+            pageIndex = MCStringUtils.tryParse(arguments[1]) - 1;
+        }
+        pageIndex = Math.max(0, Math.min(maxPages-1, pageIndex));
+
+        IFormattableTextComponent tc = new StringTextComponent("Page ").setStyle(Style.EMPTY.setColor(Color.fromHex("#FF6600")));
+        tc.append(new StringTextComponent((pageIndex + 1) + "").setStyle(Style.EMPTY.setColor(Color.fromHex("#1E89EB"))));
+        tc.append(new StringTextComponent(" of ").setStyle(Style.EMPTY.setColor(Color.fromHex("#FF6600"))));
+        tc.append(new StringTextComponent(maxPages + "").setStyle(Style.EMPTY.setColor(Color.fromHex("#1E89EB"))));
+        if(pageIndex+1 != maxPages) {
+            tc.append(new StringTextComponent(". Use \\help " + (pageIndex + 2) + " for the next page.").setStyle(Style.EMPTY.setColor(Color.fromHex("#FF6600")).setItalic(true)));
+        }
+
+        mc.ingameGUI.getChatGUI().printChatMessage(tc);
+
+        for(int i = pageIndex * 10; i < mc.commandManager.commands.size() && i < (pageIndex+1) * 10; i++) {
+            Command command = mc.commandManager.commands.get(i);
+
             mc.ingameGUI.getChatGUI().printChatMessage(
                     (new StringTextComponent("\\" + command.commandNames[0] + " ").setStyle(Style.EMPTY.setColor(Color.fromHex("#1E89EB"))))
                             .append(new StringTextComponent(command.description).setStyle(Style.EMPTY.setColor(Color.fromHex("#FF6600"))))
