c52b222c62f9f82c412bfc7ee46b50d8040bd2ea
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-12-15 20:10:41 +0100

    Build version 0.8.2

--- src/main/java/nl/_99th_dutchclient/Config.java
+++ src/main/java/nl/_99th_dutchclient/Config.java
@@ -2,7 +2,7 @@ package nl._99th_dutchclient;
 
 public class Config {
     public static String mcVersion = "1.16.5";
-    public static String clientVersion = "0.8.1";
+    public static String clientVersion = "0.8.2";
     public static String clientName = "99th_DutchClient";
 
     public static String configFile = "options99th_dutchclient.txt";
