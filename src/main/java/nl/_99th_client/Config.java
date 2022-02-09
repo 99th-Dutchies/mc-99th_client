3020cf17292aabf34610a4f8cdcd6808a7dabf8b
Jordy van der Poel <jordyvanderpoel@gmail.com>
2022-02-09 20:00:23 +0100

    Build version 0.10.0-rc3

--- src/main/java/nl/_99th_client/Config.java
+++ src/main/java/nl/_99th_client/Config.java
@@ -2,7 +2,7 @@ package nl._99th_client;
 
 public class Config {
     public static String mcVersion = "1.16.5";
-    public static String clientVersion = "0.10.0-rc2";
+    public static String clientVersion = "0.10.0-rc3";
     public static String clientName = "99th_Client";
 
     public static String[] oldConfigFiles = new String[] { "options99th_client.txt", "options99th_dutchclient.txt" };
